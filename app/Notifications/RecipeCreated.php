2e0aa5f69bfb0ef8b8345437a52db8a8ffd59184
Ilan ARFI <i.arfipro@outlook.fr>
2025-10-05 18:51:51 +0200

    recipe created notif

---
+++ app/Notifications/RecipeCreated.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RecipeCreated extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Votre recette est en ligne!')
+            ->line("Félicitations! Votre recette '{$this->dish->titre}' est maintenant en ligne sur Manger pour les nuls.")
+            ->action('Voir ma recette', url(route('dishes.show', $this->dish)))
+            ->line('Merci d\'utiliser notre application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'dish_id' => $this->dish->id,
+            'title' => $this->dish->titre,
+        ];
+    }
+}
